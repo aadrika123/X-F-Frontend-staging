e24014d146daf8c3983921800ade0830b6561730
aadrika123 <it.info@aadrikaenterprises.com>
2023-09-28 17:31:52 +0530

    csdfd

--- advertisement_build/assets/SelfAdvtUpdateForm-7b5f7b6e.js
+++
@@ -1,1 +0,0 @@
-import{h as a,j as d}from"./index-bc374d15.js";function t(){const{id:e}=a();return console.log("can edit param received id",e),d("div",{children:"SelfAdvtUpdateForm"})}export{t as default};
---
+++ advertisement_build/assets/useSetTitle-1c1d5875.js
@@ -0,0 +1,1 @@
+import{R as s,V as o,r as a}from"./index-b1f0ac39.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
