4767f38f13d8d9d230d6a3be1b83ec32b298c35a
akshay2s <akshay.aadrika@gmail.com>
2024-01-15 11:07:56 +0530

    changes

--- property_build_old/assets/GovDcbReport-a650c1da.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-d529e913.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
--- property_build_old/assets/QrCode-41c33e1c.js
+++
@@ -1,1 +0,0 @@
-import{j as t,ax as a}from"./index-d529e913.js";function x(e){return t.jsx(a,{value:e==null?void 0:e.url,size:80})}export{x as Q};
