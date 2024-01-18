86da5d903326fa37d9abf022944e34dbbc21ae82
akshay2s <akshay.aadrika@gmail.com>
2024-01-18 11:02:23 +0530

    changes

---
+++ property_build/assets/GovDcbReport-baa4aec8.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-d5453f11.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
--- property_build_old/assets/GovDcbReport-058caab7.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-dc4934ee.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
