a58d8e009eaa1f7477bd54ff22522b54597bf415
akshay2s <akshay.aadrika@gmail.com>
2024-02-03 11:11:13 +0530

    changes

---
+++ property_build/assets/QrView-38821b06.js
@@ -0,0 +1,1 @@
+import{r,j as t}from"./index-b46cbfad.js";import{Q as s}from"./QrCode-4c441b4a.js";function o(){const[e,a]=r.useState("upi://9031578239@paytm");return t.jsx(t.Fragment,{children:t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(s,{url:e,size:"300"})})})}export{o as default};
