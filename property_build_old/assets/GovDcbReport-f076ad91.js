e37e10e9409094df23f9d052cdf3db6ac990f9bd
akshay2s <akshay.aadrika@gmail.com>
2024-02-06 15:54:07 +0530

    changes

--- property_build_old/assets/GovDcbReport-f076ad91.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-99e25dc0.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
--- property_build_old/assets/RazorpayPaymentScreen-516445ff.js
+++
@@ -1,1 +0,0 @@
-import"./index-99e25dc0.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
