0b5a4fc2374a66fb2802ef1f02780c0e16012cea
akshay2s <akshay.aadrika@gmail.com>
2024-02-07 15:36:24 +0530

    changes

--- advertisement_build_old/assets/useSetTitle-f51c5621.js
+++
@@ -1,1 +0,0 @@
-import{R as s,U as o,r as a}from"./index-46a462e4.js";function c(t){const{settitleText:e}=s.useContext(o);a.useEffect(()=>{e(t)},[])}export{c as u};
---
+++ property_build/assets/GovDcbReport-938864fd.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-a9a2f934.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
