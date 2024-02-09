43b6a3d41ce55d4247ead1d37d87a1039b9cd477
akshay2s <akshay.aadrika@gmail.com>
2024-02-09 13:23:37 +0530

    chnages

--- property_build_old/assets/GovDcbReport-f2f2397e.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-b7a4e70b.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
--- property_build_old/assets/QrView-2942c568.js
+++
@@ -1,1 +0,0 @@
-import{r,j as t}from"./index-b7a4e70b.js";import{Q as s}from"./QrCode-dfbdb2bd.js";function o(){const[e,a]=r.useState("upi://9031578239@paytm");return t.jsx(t.Fragment,{children:t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(s,{url:e,size:"300"})})})}export{o as default};
