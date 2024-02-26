08230d80435e509bbed9ea8715e4918e1f65b26c
Talib Hussain <talibplay111@gmail.com>
2024-02-26 19:13:02 +0530

    updates

--- amc_tc_build_old/assets/index-zLvMM6qm.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-hZlgzPYY.js";import"./react-kJeE4wWB.js";import{P as t}from"./index-9ZDUnJrq.js";function o(){return r.jsx(t,{title:"Survey List",children:r.jsx("div",{children:"SurveyList"})})}export{o as default};
