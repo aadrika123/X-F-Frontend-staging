a273cce02950a09bbc1b831337cdbe09dcef6daa
Talib Hussain <talibplay111@gmail.com>
2024-02-27 13:26:51 +0530

    updates

---
+++ amc_tc_build/assets/index-gRDfVKtm.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-_4-Tw8Ve.js";import"./react-HXRlOr-l.js";import{P as t}from"./index-q5SJu1Jo.js";function d(){return r.jsx(t,{title:"Survey Edit",children:r.jsx("div",{children:"SurveyEdit"})})}export{d as default};
---
+++ amc_tc_build/assets/index-legacy-6QctLyY-.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-iVEgO651.js","./react-legacy-B2uxfrw8.js","./index-legacy-iPVkjGHk.js"],(function(e,t){"use strict";var n,i;return{setters:[function(e){n=e.j},null,function(e){i=e.P}],execute:function(){e("default",(function(){return n.jsx(i,{title:"Survey Edit",children:n.jsx("div",{children:"SurveyEdit"})})}))}}}));
