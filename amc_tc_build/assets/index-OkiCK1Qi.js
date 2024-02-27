4bcb4988bedbde85ce8bacec1a7a814227d6ff0e
akshay2s <akshay.aadrika@gmail.com>
2024-02-27 10:55:32 +0530

    changes

---
+++ amc_tc_build/assets/index-OkiCK1Qi.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-FJVOBgpJ.js";import"./react-HXRlOr-l.js";import{P as t}from"./index-6EniNr2p.js";function d(){return r.jsx(t,{title:"Survey Edit",children:r.jsx("div",{children:"SurveyEdit"})})}export{d as default};
