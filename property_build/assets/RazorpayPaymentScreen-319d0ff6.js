ba33c22b3f7f429b0351baa98469a0b184b02644
akshay2s <akshay.aadrika@gmail.com>
2024-02-28 11:45:25 +0530

    changes

--- property_build/assets/RazorpayPaymentScreen-319d0ff6.js
+++
@@ -1,1 +0,0 @@
-import"./index-49b8f735.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
---
+++ property_build/assets/RazorpayPaymentScreen-6c0f3816.js
@@ -0,0 +1,1 @@
+import"./index-82c72c17.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
