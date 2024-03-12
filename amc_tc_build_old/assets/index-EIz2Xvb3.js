ed7f8266f71773b175094cc7e1a2ef18094bda94
akshay2s <akshay.aadrika@gmail.com>
2024-03-12 11:53:13 +0530

    changes

--- amc_tc_build_old/assets/index-EIz2Xvb3.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-psfei14y.js";import"./react-HXRlOr-l.js";import{P as t}from"./index-02PKrgz7.js";function o(){return r.jsx(t,{title:"Survey List",children:r.jsx("div",{children:"SurveyList"})})}export{o as default};
