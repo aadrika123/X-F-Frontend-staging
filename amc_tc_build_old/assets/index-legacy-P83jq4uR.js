e1c8226e059adeb80f59f8daeeb5f88baaf50a0e
akshay2s <akshay.aadrika@gmail.com>
2024-03-12 11:52:22 +0530

    changes

---
+++ amc_tc_build_old/assets/index-legacy-P83jq4uR.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-UQcqFiW1.js","./react-legacy-B2uxfrw8.js","./index-legacy-brl6CZft.js"],(function(e,t){"use strict";var n,i;return{setters:[function(e){n=e.j},null,function(e){i=e.P}],execute:function(){e("default",(function(){return n.jsx(i,{title:"Survey Edit",children:n.jsx("div",{children:"SurveyEdit"})})}))}}}));
