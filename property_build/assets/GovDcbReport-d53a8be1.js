88b763fddb9abb2872c9860cc83c6524aefcecb3
akshay2s <akshay.aadrika@gmail.com>
2024-03-20 15:26:47 +0530

    changes

---
+++ property_build/assets/GovDcbReport-d53a8be1.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-ff983a88.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
---
+++ property_build_old_13.26/assets/Logout-aa48cbdf.js
@@ -0,0 +1,1 @@
+import{r as s,d as e,e as a,l as c}from"./index-ba7601b9.js";const n=()=>{const{notify:t}=s.useContext(e),o=a();c(),t("Logout Successfully","success"),o("/")};export{n as default};
