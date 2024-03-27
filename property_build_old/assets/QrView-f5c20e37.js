5a071aae3be23c0750672a89c744df99ec74387a
akshay2s <akshay.aadrika@gmail.com>
2024-03-27 10:08:53 +0530

    changes

--- property_build_old/assets/QrView-f5c20e37.js
+++
@@ -1,1 +0,0 @@
-import{r,j as t}from"./index-4d92e3d9.js";import{Q as s}from"./QrCode-3f602956.js";function o(){const[e,a]=r.useState("upi://9031578239@paytm");return t.jsx(t.Fragment,{children:t.jsx("div",{className:"flex justify-center items-center",children:t.jsx(s,{url:e,size:"300"})})})}export{o as default};
--- property_build_old/assets/RazorpayPaymentScreen-a00c4ee2.js
+++
@@ -1,1 +0,0 @@
-import"./index-4d92e3d9.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
