a6c3f0edcc64888a3ff0537a4fa24d952f347502
Talib Hussain <talibplay111@gmail.com>
2024-05-08 15:34:23 +0530

    changes

---
+++ property_build/assets/GovDcbReport-0ff13ccc.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-be48fc6e.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
--- property_build_old/assets/GovDcbReport-5c39a40c.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-cfb90537.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
