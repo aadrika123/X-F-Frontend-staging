c449fabdf7fb5d60f4054246996cb36e73719c1e
Talib Hussain <talibplay111@gmail.com>
2024-05-11 12:05:44 +0530

    changes

---
+++ property_build/assets/GovDcbReport-cf6b385d.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-99d0da59.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
--- property_build/assets/RazorpayPaymentScreen-41babaf7.js
+++
@@ -1,1 +0,0 @@
-import"./index-5be48e5d.js";let o=window.localStorage.getItem("token");console.log("token at basic details is post method...",o);
