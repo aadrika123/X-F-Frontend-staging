cc03fba458eea6d3554aaeb6acf6ce57867179f2
akshay2s <akshay.aadrika@gmail.com>
2024-09-12 10:02:09 +0530

    changes

---
+++ property_build/assets/RazorpayPaymentScreen-72beff6c.js
@@ -0,0 +1,1 @@
+import"./index-13cba9be.js";window.localStorage.getItem("token");
--- property_build_old/assets/GovDcbReport-1a92c627.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-9035172f.js";const t=()=>o.jsx("div",{children:"GovDcbReport"});export{t as default};
